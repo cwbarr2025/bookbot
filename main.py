400108ce59e93f6c7eb96057c94c9800ea57445c
cwbarr <cameronbarr0114@gmail.com>
2025-10-23 07:04:44 -0600

    feature: added sort_counts function to make list of dictionaries sorted by character count

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import number_words , number_characters
+from stats import number_words , number_characters , sort_counts
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -7,8 +7,20 @@ def get_book_text(filepath):
 def main():
     char_num = {}
     text = get_book_text("books/frankenstein.txt")
-    print(f"Found {number_words(text)} total words")
+    #print(f"Found {number_words(text)} total words")
     char_num = number_characters(text)
-    print(char_num)
+    sorted_list = sort_counts(char_num)
+    #print(sorted_list)
+
+    print("============ BOOKBOT ============\n" #\n tells print to go to a new line in the terminal/output
+          "Analyzing book found at books/frankenstein.txt...\n"
+          "----------- Word Count ----------\n"
+          f"Found {number_words(text)} total words")
+    print("--------- Character Count -------")
+    for dict in sorted_list: #iterates through my sorted list of dictionaries (char: .. , num: ..)
+        if dict["char"].isalpha(): #checks character tied to the key "char" and returnes True if it is an alphebatical character
+            print(f"{dict["char"]}: {dict["num"]}") #prints the char and the num to screen
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -16,3 +16,15 @@ def number_characters(text):
             else:
                 char_num[char] = 1 # creates the key and sets the value to 1 if it does not exist yet
     return char_num
+
+def sort_on(chars): #need to make helper function (see example on lesson page)
+   return chars["num"]
+
+def sort_counts(char_dict):
+    sorted_list = []
+    for key in char_dict:
+        new_dict = {} # reset the new_dict to be empty for each iteration
+        new_dict = {"char" : key , "num" : char_dict[key]} # creates a dictionary with "char" and "num" as keys
+        sorted_list.append(new_dict) # adds the dictionary to my list of dictionairies
+    sorted_list.sort(reverse=True , key=sort_on) # reverse=True sorts it from highest to lowest value , key=sort_on sends it to helper function which tells it to sort via "num"?
+    return sorted_list
